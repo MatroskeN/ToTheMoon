75d3e76529a250bd0327215e0fc4003784abdca0
Vlad <44238649+MatroskeN@users.noreply.github.com>
2022-09-23 14:01:13 +0300

    fade scroll fix

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,109 +1,16 @@
-(function() {
-    "use strict";
-    /*[pan and well CSS scrolls]*/
-    let pnls = document.querySelectorAll('section').length,
-        scdir, hold = false;
+let page = document.querySelector('.darkTheme');
 
-    function _scrollY(obj) {
-        let slength, plength, pan, step = 100,
-            vh = window.innerHeight / 100,
-            vmin = Math.min(window.innerHeight, window.innerWidth) / 100;
-        if ((this !== undefined && this.id === 'well') || (obj !== undefined && obj.id === 'well')) {
-            pan = this || obj;
-            plength = parseInt(pan.offsetHeight / vh);
-        }
-        if (pan === undefined) {
-            return;
-        }
-        plength = plength || parseInt(pan.offsetHeight / vmin);
-        slength = parseInt(pan.style.transform.replace('translateY(', ''));
-        if (scdir === 'up' && Math.abs(slength) < (plength - plength / pnls)) {
-            slength = slength - step;
-        } else if (scdir === 'down' && slength < 0) {
-            slength = slength + step;
-        } else if (scdir === 'top') {
-            slength = 0;
-        }
-        if (hold === false) {
-            hold = true;
-            pan.style.transform = 'translateY(' + slength + 'vh)';
-            setTimeout(function() {
-                hold = false;
-            }, 1000);
-        }
+window.addEventListener("wheel", function (event) {
+    if (event.deltaY < 0)
+    {
+        page.style.background = '#0F1011';
+        page.classList.remove('lightTheme');
+        document.querySelector('.sectionFootNote').innerHTML = 'To The Moon - <span>UK\'s fastest</span> <br> national data speed network'
     }
-    /*[swipe detection on touchscreen devices]*/
-    function _swipe(obj) {
-        let swdir,
-            sX,
-            sY,
-            dX,
-            dY,
-            threshold = 100,
-            /*[min distance traveled to be considered swipe]*/
-            slack = 50,
-            /*[max distance allowed at the same time in perpendicular direction]*/
-            alT = 500,
-            /*[max time allowed to travel that distance]*/
-            elT, /*[elapsed time]*/
-            stT; /*[start time]*/
-        obj.addEventListener('touchstart', function(e) {
-            let tchs = e.changedTouches[0];
-            swdir = 'none';
-            sX = tchs.pageX;
-            sY = tchs.pageY;
-            stT = new Date().getTime();
-            //e.preventDefault();
-        }, false);
-
-        obj.addEventListener('touchmove', function(e) {
-            e.preventDefault(); /*[prevent scrolling when inside DIV]*/
-        }, false);
-
-        obj.addEventListener('touchend', function(e) {
-            let tchs = e.changedTouches[0];
-            dX = tchs.pageX - sX;
-            dY = tchs.pageY - sY;
-            elT = new Date().getTime() - stT;
-            if (elT <= alT) {
-                if (Math.abs(dX) >= threshold && Math.abs(dY) <= slack) {
-                    swdir = (dX < 0) ? 'left' : 'right';
-                } else if (Math.abs(dY) >= threshold && Math.abs(dX) <= slack) {
-                    swdir = (dY < 0) ? 'up' : 'down';
-                }
-                if (obj.id === 'well') {
-                    if (swdir === 'up') {
-                        scdir = swdir;
-                        _scrollY(obj);
-                    } else if (swdir === 'down' && obj.style.transform !== 'translateY(0)') {
-                        scdir = swdir;
-                        _scrollY(obj);
-
-                    }
-                    e.stopPropagation();
-                }
-            }
-        }, false);
+    else if (event.deltaY > 0)
+    {
+        page.style.background = '#E7E7E8';
+        page.classList.add('lightTheme');
+        document.querySelector('.sectionFootNote').innerHTML = 'Choose one of our <br><span>flex bundles</span>'
     }
-    /*[assignments]*/
-    let well = document.getElementById('well');
-    well.style.transform = 'translateY(0)';
-    well.addEventListener('wheel', function(e) {
-        if (e.deltaY < 0) {
-            scdir = 'down';
-        }
-        if (e.deltaY > 0) {
-            scdir = 'up';
-        }
-        e.stopPropagation();
-    });
-    well.addEventListener('wheel', _scrollY);
-    _swipe(well);
-    let tops = document.querySelectorAll('.top');
-    for (let i = 0; i < tops.length; i++) {
-        tops[i].addEventListener('click', function() {
-            scdir = 'top';
-            _scrollY(well);
-        });
-    }
-})();
+}, true)
